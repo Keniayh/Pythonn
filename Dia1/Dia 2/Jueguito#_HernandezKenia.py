6787756cc0badb0955a4279868da481a7209495a
Kenia <keniayhdiaz@gmail.com>
2024-01-29 06:41:17 -0500

    SerieFibonacci

---
+++ Dia1/Dia 2/Jueguito#_HernandezKenia.py
@@ -0,0 +1,35 @@
+from random import randint
+
+print("""
+      Bienvenido!!! 
+      El juego consiste en adivinar un número secreto del 1 al 100.
+      Tienes 10 intentos para adivinar el número de lo contrario se cerrara el juego.
+       """)
+
+numeroSecreto = randint(1,100)
+contar = 10
+intentos = 1
+
+
+while (contar != 0):
+    print("Intento # ",intentos)
+    print("Ingresa un número mayor o igual a 1 y menor o igual a 100")
+    num = int(input())
+    
+    if intentos==10:
+        print("Lo lamento, No tienes mas intentos")
+        break
+    else:
+        if (num == numeroSecreto):
+            print("Felicidades has adivinado el número.")
+            print("Adivinaste el número en ",intentos, " intentos.")
+            print("Fin del juego.")
+            contar = 0
+        elif (num < numeroSecreto):
+            print("El número secreto es mayor al número ingresado.")
+            contar -= 1
+            intentos += 1
+        elif (num > numeroSecreto):
+            print("El número secreto es menor al número ingresado.")
+            contar -= 1
+            intentos+=1
---
+++ Dia1/Dia 2/SerieFibo_HernandezKenia.py
@@ -0,0 +1,35 @@
+tex = 1
+while tex == 1:
+    print("""Bienvenido!
+        
+    A continuación ingresaras un número para determinar hasta que termino de la secuencia deseas generar.
+        
+    La serie de Fibonacci es, en matemáticas, una secuencia ordenada de números descrita por Leonardo de Pisa, 
+    matemático italiano del siglo XIII""")
+ 
+    num = int(input("Ingresa un número mayor a 1 para determinar hasta que termino de la secuencia deseas generar: "))
+    if num == 1:
+        print("0")
+        break
+
+    def serieFibo(n): 
+        num = n
+        a = 0
+        b = 1
+        print("0")
+        print("1")
+        for i in range (n-2):
+            c = a + b
+            a = b
+            b = c 
+            print(c)
+ 
+    serieFibo(int(num))
+
+    print("Desea seguir explorando la serie de Fibonacci?, tenga en cuenta que 1 = si y 0 = no.")
+    texto = input()
+    
+    if texto == "0":
+        tex = 0
+    else:
+        tex = 1
