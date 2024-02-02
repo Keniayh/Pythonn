d994e27a5b2bbd7b7f631ce61fcf2a994a0ae9c1
kenia <keniayhdiaz@gmail.com>
2024-02-02 06:45:29 -0500

    exeercise

---
+++ Dia6/Ejercicio3_HernandezKenia.py
@@ -0,0 +1,14 @@
+import math
+
+def Colision(bola1, bola2):
+    x1, y1, r1 = bola1
+    x2, y2, r2 = bola2
+
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+    return distance <= r1 + r2
+
+bola1 = (0, 0, 5)
+bola2 = (8, 0, 3)
+
+print(Colision(bola1, bola2))
