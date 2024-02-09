7cf316b4df5406bca6011b48e27fcca9891ae21c
Kenia <keniayhdiaz@gmail.com>
2024-02-08 23:59:55 -0500

    a

--- Dia8/Crud.py
+++ Dia8/Crud.py
@@ -1,24 +1,36 @@
 import json
 
-#Creación
+# Función para agregar un nuevo cliente
+def agregar_cliente(cliente):
+    datos_ventas["ventas"]["clientes"].append(cliente)
 
-with open ('data.json', 'r') as archivoUno:
-     eje = json.load(archivoUno)
+# Función para actualizar información de un cliente por su ID
+def actualizar_cliente(id_cliente, nueva_informacion):
+    for cliente in datos_ventas["ventas"]["clientes"]:
+        if cliente["id"] == id_cliente:
+            cliente.update(nueva_informacion)
+            break
 
+# Función para eliminar un cliente por su ID
+def eliminar_cliente(id_cliente):
+    datos_ventas["ventas"]["clientes"] = [cliente for cliente in datos_ventas["ventas"]["clientes"] if cliente["id"] != id_cliente]
 
+# Ejemplo de uso
+nuevo_cliente = {"id": 11, "nombre": "Nuevo", "apellido1": "Cliente", "ciudad": "Barcelona", "categoría": 150}
+agregar_cliente(nuevo_cliente)
 
-#Abrir un archivo externo (JSON) y convertirlo a DICCIONARIO
+print("Clientes después de agregar:")
+print(datos_ventas["ventas"]["clientes"])
 
-miVariable = open('info.json')
-miJson=json.load(miVariable)
+id_cliente_a_actualizar = 4
+nueva_informacion_cliente = {"nombre": "NuevoNombre", "ciudad": "NuevaCiudad"}
+actualizar_cliente(id_cliente_a_actualizar, nueva_informacion_cliente)
 
-x = int(input('Ingresa un entero:'))
-miJson['entero']=x
-print(miJson)
+print("\nClientes después de actualizar:")
+print(datos_ventas["ventas"]["clientes"])
 
-#Convertir diccionario a Json
-jsonFinal= json.dumps(miJson)
+id_cliente_a_eliminar = 5
+eliminar_cliente(id_cliente_a_eliminar)
 
-#Cojer el JSON y guardarlo en un archivo
-with open('info.json','w') as outfile:
-    outfile.write(jsonFinal)    
+print("\nClientes después de eliminar:")
+print(datos_ventas["ventas"]["clientes"])
